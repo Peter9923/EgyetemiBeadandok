513fde1a7032e98cf7dff1d0582f0c4e3c408669
SzilagyiPeter <66031693+Peter9923@users.noreply.github.com>
2020-11-30 18:54:26 +0100

    Add files via upload

---
+++ Lánckód/LáncKód/LáncKód/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LáncKód
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PictureManager pm = new PictureManager();
+            pm.BlackPatch();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lánckód/LáncKód/LáncKód/Show.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LáncKód
+{
+    static class Show
+    {
+        public static void DrawMap(string[,] _map)
+        {
+            for (int i = 0; i < _map.GetLength(0); i++)
+            {
+                for (int j = 0; j < _map.GetLength(1); j++)
+                {
+                    if (_map[i, j] == "+" || _map[i, j] == "-" || _map[i, j] == "|")
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(_map[i, j]);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(_map[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
